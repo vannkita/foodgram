d7fbee343fc8b25f21f887de1a9ba16c07872d8a
NikitaV <idvvm@yandex.ru>
2025-08-13 22:51:32 +0300

    1

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -11,15 +11,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', '')
+SECRET_KEY = os.environ['SECRET_KEY']
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG') == 'True'
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
-ALLOWED_HOSTS = [
-    'greatfood.zapto.org', '127.0.0.1', 'localhost', '94.142.139.148']
-
-AUTH_USER_MODEL = 'users.MyUser'
+if DEBUG:
+    ALLOWED_HOSTS.append('localhost')
+    ALLOWED_HOSTS.append('127.0.0.1')
 
 # Application definition
 
