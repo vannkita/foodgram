e50d3abf5ca08e9d484ec89855512814663b11fa
NikitaV <idvvm@yandex.ru>
2025-08-13 23:09:18 +0300

    1

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -3,6 +3,9 @@ import os
 from pathlib import Path
 
 from .constants import DEFAULT_PAGE_SIZE
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -12,10 +15,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-fallback-key-for-ci')
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 if DEBUG:
     ALLOWED_HOSTS.append('localhost')
