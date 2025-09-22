57a2daa37127ab361ff6e600ccac6ea42cd22928
NikitaV <idvvm@yandex.ru>
2025-09-22 22:10:23 +0300

    123

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -90,8 +90,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-# 'django.middleware.csrf.CsrfViewMiddleware',  # Отключено
+    'django.middleware.common.CommonMiddleware', # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
