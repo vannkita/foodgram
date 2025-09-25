e1cd493caaa3e11ed76ba90d6cca5ea8036396a9
NikitaV <idvvm@yandex.ru>
2025-09-25 21:08:59 +0300

    правки 2

--- backend/foodgram/recipes/management/commands/import_ingredients.py
+++ backend/foodgram/recipes/management/commands/import_ingredients.py
@@ -1,6 +1,9 @@
 import csv
+
 from django.core.management.base import BaseCommand
+
 from recipes.models import Ingredient
+
 
 class Command(BaseCommand):
     help = 'Импорт ингредиентов из CSV файла'
