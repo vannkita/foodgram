98e236457dca6ed435caffd7bdccd4ee41d184fe
NikitaV <idvvm@yandex.ru>
2025-09-25 21:29:10 +0300

    правки 2

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -1,13 +1,14 @@
 from django.contrib.auth import get_user_model
 from django.templatetags.static import static
+
 from drf_base64.fields import Base64ImageField
 from rest_framework import serializers
 
 from foodgram.constants import (
-    MAX_AMOUNT,
-    MAX_COOKING_TIME,
     MIN_AMOUNT,
-    MIN_COOKING_TIME
+    MAX_AMOUNT,
+    MIN_COOKING_TIME,
+    MAX_COOKING_TIME,
 )
 from recipes.models import Ingredient, Recipe, RecipeIngredient, Tag
 
